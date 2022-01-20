dc6cd823cf5385462f67a4b077ca47401eab0a0b
Yunhyunjo <gotqlc147@likelion.org>
2022-01-20 16:59:36 +0900

    0120 - AC

---
+++ Inflearn/13. 가장 많이 사용된 자릿수.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int arr[10], max = -2147000000, res;
+
+    cin >> s;
+
+    for (char c : s) {
+        arr[c - 48]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (max <= arr[i]) {
+            max = arr[i];
+            res = i;
+        }
+    }
+    cout << res;
+
+    return 0;
+}
+
