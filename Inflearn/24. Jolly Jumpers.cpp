e3d4ed3c8e04105791ae1d2f48c4c0250734c231
Yunhyunjo <gotqlc147@likelion.org>
2022-01-27 21:58:11 +0900

    0127 - AC

---
+++ Inflearn/24. Jolly Jumpers.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, p, now;
+    
+    cin >> n;
+    vector <int> v(n);
+    vector <int> vv(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    for (int i = 1; i < n; i++) {
+        int a = abs(v[i] - v[i - 1]);
+        if (a >= n) {
+            cout << "NO";
+            return 0;
+        }
+        vv[a]++;
+    }
+
+    for (int i = 1; i < n; i++) {
+        if (vv[i] == 0) {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+    return 0;
+}
