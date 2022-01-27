222ac29e46ea711148f3f3fa2efc57406b980e68
Yunhyunjo <gotqlc147@likelion.org>
2022-01-27 21:44:50 +0900

    0127 - AC

---
+++ Inflearn/23. 연속 부분 증가수열.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, num, max = 0, p = -2147000000, cnt = 0;
+    
+    cin >> n;
+    while (n--)
+    {
+        cin >> num;
+        if (p <= num) cnt++;
+        else {
+            if (max < cnt) max = cnt;
+            cnt = 1;
+        }
+        p = num;
+    }
+
+    if (max == 0) max =  cnt;
+    cout << max;
+
+    return 0;
+}
