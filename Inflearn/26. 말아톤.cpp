8b1e5cdef30c0ab7b81c098c5d20056bba304ba5
Yunhyunjo <gotqlc147@likelion.org>
2022-01-29 19:21:48 +0900

    0129 - AC

---
+++ Inflearn/26. 말아톤.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+
+	cin >> n;
+	vector <int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	cout << 1 << " ";
+	for (int i = 1; i < n; i++) {
+		int cnt = 0;
+		for (int j = i - 1; j >= 0; j--) {
+			if (v[i] <= v[j]) cnt++;
+		}
+		cout << cnt + 1 << " ";
+	}
+
+	return 0;
+}
