2b47d60259fdff48e4e3336d199a6aab8a14c61a
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 12:37:32 +0900

    0131 - AC

--- Inflearn/29, 30. 3의 개수.cpp
+++ Inflearn/29, 30. 3의 개수.cpp
@@ -1,8 +1,14 @@
 ﻿#include <iostream>
-#include <vector>
 
 using namespace std;
 
+/*
+각 자리의 수가 3이 총 몇 번 오는 지를 구해서 더해줘야 한다.
+ex) 5367, 현재 자리수 6
+lt = 53이고 rt = 7
+6은 3보다 크므로 lt + 1 = 54 번 그리고 6이 10의 자리수이므로 * 10 해서 십의 자리에서는 3이 총 540번 나온다.
+
+*/
 int main() {
 
 	ios::sync_with_stdio(0);
