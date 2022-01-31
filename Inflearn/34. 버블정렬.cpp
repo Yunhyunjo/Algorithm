842ca344f0c7a5caaea9eff2d1bea6d317421b68
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 13:16:16 +0900

    0131 - AC

---
+++ Inflearn/34. 버블정렬.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+
+	cin >> n;
+	vector <int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		for (int j = 1; j < n - i; j++) {
+			if (v[j] < v[j - 1]) {
+				tmp = v[j];
+				v[j] = v[j - 1];
+				v[j-1] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << " ";
+	}
+	
+
+	return 0;
+}
