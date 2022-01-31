118d78de5fdaf43e242bf431f7a49a32facf9ccc
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 14:19:16 +0900

    0131 - AC

---
+++ Inflearn/36. 삽입정렬.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+
+	cin >> n;
+	vector <int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	for (int i = 1; i < n; i++) {
+		int tmp = v[i];
+		int j = i - 1;
+		for (j; j >= 0; j--) {
+			if (tmp < v[j]) {
+				v[j + 1] = v[j];
+			}
+			else break;
+		}
+		v[j + 1] = tmp;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << " ";
+	}
+
+	return 0;
+}
