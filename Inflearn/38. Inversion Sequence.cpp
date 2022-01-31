3359abab9f9e7b17e891c20d7235e6c73fcc19f5
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 16:00:52 +0900

    0131 - AC

---
+++ Inflearn/38. Inversion Sequence.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int s, n;
+
+	cin >> n;
+	vector <int> is(n, 0);
+	vector <int> v(n, 0);
+
+	int a, j;
+	for (int i = 0; i < n; i++) {
+		cin >> is[i];
+		v[i] = i + 1;
+	}
+
+	for (int i = n - 2; i >= 0; i--) {
+		for (j = i; j < i + is[i]; j++) {
+			v[j] = v[j + 1];
+		}
+		v[j] = i + 1;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << " ";
+	}
+
+	return 0;
+}
