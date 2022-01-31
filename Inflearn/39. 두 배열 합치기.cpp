62a467d2c9a36b197ea0bd550da6913d409ebf2e
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 16:18:17 +0900

    0131 - AC

---
+++ Inflearn/39. 두 배열 합치기.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int s, n, m;
+
+	cin >> n;
+	vector <int> a(n, 0);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	cin >> m;
+	vector <int> b(m, 0);
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+
+	vector <int> v(n + m);
+	int i = 0, j = 0, k = 0;
+	while (i < n && j < m) {
+		if (a[i] < b[j]) v[k++] = a[i++];
+		else v[k++] = b[j++];
+	}
+
+	while (i < n) v[k++] = a[i++];
+	while (j < m) v[k++] = b[j++];
+
+	for (int i = 0; i < n + m; i++) {
+		cout << v[i] << " ";
+	}
+
+	return 0;
+}
---
+++ Inflearn/40. 교집합(투포인터 알고리즘).cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int s, n, m;
+
+	cin >> n;
+	vector <int> a(n, 0);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+
+	cin >> m;
+	vector <int> b(m, 0);
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+	sort(b.begin(), b.end());
+
+	vector <int> v;
+	int i = 0, j = 0, k = 0;
+	while (i < n && j < m) {
+		if (a[i] < b[j]) i++;
+		else if (a[i] > b[j]) j++;
+		else {
+			v.push_back(a[i++]);
+			j++;
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+
+	return 0;
+}
