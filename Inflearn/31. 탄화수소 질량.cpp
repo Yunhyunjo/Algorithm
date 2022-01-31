aa2789dadecc30eb724ea78cf3d0bb683deaec7a
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 12:54:48 +0900

    0131 - AC

---
+++ Inflearn/31. 탄화수소 질량.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	string s, str = "";
+	int i, w = 0;
+
+	cin >> s;
+	
+	if (s[1] != 'H') {
+		i = 1;
+		while (s[i] != 'H') {
+			str += s[i++];
+		}
+		w = 12 * stoi(str);
+		i++;
+		str = "";
+	}
+	else {
+		w = 12;
+		i = 2;
+	}
+
+	if (s.size() == i) cout << w + 1;
+	else {
+		for (i; i < s.size(); i++) {
+			str += s[i];
+		}
+		cout << w + stoi(str);
+	}
+
+
+	return 0;
+}
