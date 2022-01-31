4f2961f4e11bfa9ae87351c9fe52d71f90ce4d6a
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 13:09:40 +0900

    0131 - AC

---
+++ Inflearn/33. 3등의 성적은.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+
+	cin >> n;
+	vector <int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		int k = i;
+		for (int j = i + 1; j < n; j++) {
+			if (v[k] > v[j]) k = j;
+		}
+		int tmp = v[i];
+		v[i] = v[k];
+		v[k] = tmp;
+	}
+	int i = n - 1, s = 1;
+	while (s != 3) {
+		if (v[i - 1] != v[i]) {
+			s++;
+		}
+		i--;
+	}
+
+	cout << v[i];
+	
+
+	return 0;
+}
