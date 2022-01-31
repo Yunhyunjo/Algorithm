39a0c161e5c87dc3e5d4ffa7fa1e533550961343
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 13:01:38 +0900

    0131 - AC

---
+++ Inflearn/32. 선택정렬.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+
+	cin >> n;
+	vector <int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		int k = i;
+		for (int j = i + 1; j < n; j++) {
+			if (v[k] > v[j]) k = j;
+		}
+		int tmp = v[i];
+		v[i] = v[k];
+		v[k] = tmp;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << " ";
+	}
+	
+
+	return 0;
+}
