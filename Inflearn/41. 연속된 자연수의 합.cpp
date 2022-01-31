5bed6cf8d2722e0824f370d0743b832da7f12cd7
Yunhyunjo <gotqlc147@likelion.org>
2022-01-31 16:58:34 +0900

    0131 - AC

---
+++ Inflearn/41. 연속된 자연수의 합.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, cnt = 0, num = 2, hap = 3, r;
+
+	cin >> n;
+
+	while (hap <= n) {
+		r = n - hap;
+		if (r % num == 0) {
+			int a = r / num;
+			cout << a + 1;
+			for (int i = 2; i <= num; i++) {
+				cout << " + " << a + i;
+			}
+			cout << " = " << n << "\n";
+			cnt++;
+		}
+		num++;
+		hap += num;
+	}
+
+	cout << cnt;
+	
+	
+	return 0;
+}
+
+/*
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, cnt = 0;
+
+	cin >> n;
+
+	for (int i = n / 2 + 1; i > 0; i--) {
+		int num = i;
+		int sum = num;
+		while (num--) {
+			if (sum > n) break;
+			else if (sum < n) sum += num;
+			else {
+				cout << num + 1;
+				for (int j = num + 2; j <= i; j++) {
+					cout << " + " << j;
+				}
+				cout << " = " << n << "\n";
+				cnt++;
+				break;
+			}
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
+*/
---
+++ Inflearn/42. 이분검색.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, m, mid, lt = 0, rt;
+
+	cin >> n >> m;
+	
+	vector <int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	rt = n - 1;
+	while (lt <= rt) {
+		mid = (lt + rt) / 2;
+		if (v[mid] > m) rt = mid - 1;
+		else if (v[mid] < m) lt = mid + 1;
+		else break;
+	}
+
+	cout << mid + 1;
+	
+	return 0;
+}
