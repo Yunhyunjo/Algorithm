c48287d9c5cb7641b0512754a2fa15d4f5231c37
Yunhyunjo <gotqlc147@likelion.org>
2022-02-01 13:28:47 +0900

    0201 - AC

---
+++ Inflearn/45. 공주 구하기.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, m;
+
+	cin >> n >> m;
+	
+	queue <int> q;
+
+	for (int i = 0; i < n; i++) {
+		q.push(i + 1);
+	}
+	
+	int c = 0;
+	while (q.size() != 1) {
+		c++;
+		int a = q.front();
+		q.pop();
+
+		if (c % m != 0) q.push(a);
+	}
+
+	cout << q.front();
+	
+	return 0;
+}
