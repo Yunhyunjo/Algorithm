d48845b85147477148901a52b41ec7e545cfd754
Yunhyunjo <gotqlc147@likelion.org>
2022-02-01 15:03:10 +0900

    0201 - AC

---
+++ Inflearn/47. 봉우리.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[52][52];
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, cnt = 0;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			bool flag = true;
+			for (int k = 0; k < 4; k++) {
+				if (arr[i][j] <= arr[i + dx[k]][j + dy[k]]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				cnt++;
+				j++;
+			}
+		}
+	}
+
+	cout << cnt;
+	
+	return 0;
+}
