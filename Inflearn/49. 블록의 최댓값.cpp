d42a4e370c22f8fe4d424a61aa82f9d41f710cce
Yunhyunjo <gotqlc147@likelion.org>
2022-02-02 16:16:07 +0900

    0202 - AC/REF

---
+++ Inflearn/49. 블록의 최댓값.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[2][10];
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, cnt = 0;
+	cin >> n;
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (arr[0][i] < arr[1][j]) cnt += arr[0][i];
+			else cnt += arr[1][j];
+		}
+	}
+	
+	cout << cnt;
+
+	return 0;
+}
---
+++ Inflearn/50, 51. 영지(territory) 선택.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int v[701][701];
+int dp[701][701];
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int h, w, maxi = 0, h1, w1, tmp;
+	cin >> h >> w;
+
+	for (int i = 1; i <= h; i++) {
+		for (int j = 1; j <= w; j++) {
+			cin >> v[i][j];
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i-1][j-1] + v[i][j];
+		}
+	}
+
+	cin >> h1 >> w1;
+	for (int i = h1; i <= h; i++) {
+		for (int j = w1; j <= w; j++) {
+			tmp = dp[i][j] - dp[i - h1][j] - dp[i][j - w1] + dp[i - h1][j - w1];
+			if (tmp > maxi) maxi = tmp;
+		}
+	}
+
+	cout << maxi;
+
+	return 0;
+}
