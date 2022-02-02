a0009376abd09840aca8f7b7a6d964a7393f6a16
Yunhyunjo <gotqlc147@likelion.org>
2022-02-02 21:22:06 +0900

    0202 - AC/REF

---
+++ Inflearn/53. K진수 출력.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+string converter(int n, int k) {
+	stack <char> s;
+
+	string ss = "0123456789ABCDEF";
+
+	while (n >= k) {
+		int i = n % k;
+		s.push(ss[i]);
+		n /= k;
+	}
+	s.push(ss[n]);
+	
+
+	string str = "";
+	while (!s.empty()) {
+		str += s.top();
+		s.pop();
+	}
+
+	return str;
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+
+	cout << converter(n, k);
+
+	return 0;
+}
+
+
+
