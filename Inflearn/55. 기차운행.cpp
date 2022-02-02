1f09e75e87c89e1f35beb594e799e3dd511364f0
Yunhyunjo <gotqlc147@likelion.org>
2022-02-02 21:44:02 +0900

    0202 - AC

---
+++ Inflearn/55. 기차운행.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, now = 1;
+	cin >> n;
+
+	vector <int> v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	stack <int> s;
+	string str = "";
+	for (int i = 0; i < n; i++) {
+		s.push(v[i]);
+		str += 'P';
+		while (!s.empty() && s.top() == now) {
+			s.pop();
+			str += 'O';
+			now++;
+		}
+	}
+
+	if (s.empty()) cout << str;
+	else cout << "impossible";
+
+	return 0;
+}
+
+
+
