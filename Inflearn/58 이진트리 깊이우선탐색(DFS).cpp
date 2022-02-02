b0c7b5a50b70b4e67e9f3639c36864f11d466ec5
Yunhyunjo <gotqlc147@likelion.org>
2022-02-02 23:57:58 +0900

    0202 - AC/REF

---
+++ Inflearn/58 이진트리 깊이우선탐색(DFS).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void dfs(int n) {
+
+	if (n > 7) return;
+
+	cout << n << " ";  // 전위순회 출력
+	dfs(n*2);
+	//cout << n << " ";  // 중위순회 출력
+	dfs(n * 2 + 1);
+	//cout << n << " ";  // 후위순회 출력
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	dfs(1);
+
+	return 0;
+}
+
+
+
