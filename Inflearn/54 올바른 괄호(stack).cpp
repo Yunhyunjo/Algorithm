1b27277d9020d0cae5c418fcd31a4e7802f0d057
Yunhyunjo <gotqlc147@likelion.org>
2022-02-02 21:36:29 +0900

    0202 - AC

---
+++ Inflearn/54 올바른 괄호(stack).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	string s;
+	cin >> s;
+
+	stack <char> ss;
+	int i = 1;
+	
+	for(int i = 0; i < s.size(); i++){
+		if (s[i] == '(') ss.push('(');
+		else {
+			if (ss.empty()) {
+				cout << "NO";
+				return 0;
+			}
+			else ss.pop();
+		}
+	}
+
+	if (ss.empty()) cout << "YES";
+	else cout << "NO";
+
+	return 0;
+}
+
+
+
