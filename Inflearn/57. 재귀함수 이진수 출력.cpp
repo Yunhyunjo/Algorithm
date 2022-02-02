37b35ce501be99f11ed668da5633b6375a38ddc8
Yunhyunjo <gotqlc147@likelion.org>
2022-02-02 21:56:27 +0900

    0202 - AC

---
+++ Inflearn/57. 재귀함수 이진수 출력.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void print(int n) {
+
+	if (n == 0) return;
+	print(n / 2);
+	cout << n % 2;
+
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	print(n);
+
+	return 0;
+}
+
+
+
