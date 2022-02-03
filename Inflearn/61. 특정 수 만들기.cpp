b70bcdbde41fbb1a8ff8266360b0678205cd73b0
Yunhyunjo <gotqlc147@likelion.org>
2022-02-03 17:41:15 +0900

    0203 - AC

---
+++ Inflearn/61. 특정 수 만들기.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[11];
+int n, m, cnt = 0;
+
+void dfs(int L, int sum) {
+
+	if (L == n + 1) {
+		if(sum == m) cnt++;
+		return;
+	}
+	dfs(L + 1, sum + arr[L]);
+	dfs(L + 1, sum);
+	dfs(L + 1, sum - arr[L]);
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+
+	dfs(1, 0);
+	if (cnt == 0) cout << -1;
+	else cout << cnt;
+
+	return 0;
+}
