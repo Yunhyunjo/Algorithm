e1bbb7a7923f7169016ae5c3503d6a98402f236a
Yunhyunjo <gotqlc147@likelion.org>
2022-02-04 17:13:34 +0900

    0204 - AC

---
+++ Inflearn/73. 최대힙(priority_queue).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int n;
+	priority_queue <int> q;
+	while (1) {
+		cin >> n;
+		if (n == 0) {
+			if (q.empty()) cout << -1 << "\n";
+			else {
+				cout << q.top();
+				q.pop();
+			}
+		}
+		else if (n == -1) break;
+		else {
+			q.push(n);
+		}
+	}
+
+	return 0;
+}
