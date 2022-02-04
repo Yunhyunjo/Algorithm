a8fe07a10f67072dedc9a5418370b8934828ee35
Yunhyunjo <gotqlc147@likelion.org>
2022-02-04 14:33:59 +0900

    0204 - AC

---
+++ Inflearn/63 인접행렬(가중치 방향그래프).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, m, arr[20][20] = { 0, }, a, b, c;
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b >> c;
+		arr[a - 1][b - 1] = c;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	
+
+	return 0;
+}
