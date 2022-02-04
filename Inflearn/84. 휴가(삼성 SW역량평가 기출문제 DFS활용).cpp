565e33de5b176023d54c13ece9d27444ba7a5b05
Yunhyunjo <gotqlc147@likelion.org>
2022-02-05 03:09:49 +0900

    0205 - AC/REF

---
+++ Inflearn/84. 휴가(삼성 SW역량평가 기출문제 DFS활용).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int t[15], p[15];
+int n, maxi = 0;
+
+void dfs(int k, int sum) {
+
+	if (k > n) return;
+	else {
+		if (maxi < sum) maxi = sum;
+	}
+
+	for (int i = k; i < n; i++) {
+		dfs(i + t[i], sum + p[i]);
+	}
+}
+
+int main() {
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> t[i] >> p[i];
+	}
+	
+	dfs(0, 0);
+
+	cout << maxi;
+
+	return 0;
+}
