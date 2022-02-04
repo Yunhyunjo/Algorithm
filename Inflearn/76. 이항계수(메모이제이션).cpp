fc9dbf380b3303943efa61574bb14da27dfd6736
Yunhyunjo <gotqlc147@likelion.org>
2022-02-04 22:35:05 +0900

    0204 - AC/REF

---
+++ Inflearn/76. 이항계수(메모이제이션).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[21][21];
+
+int memo(int x, int y) {
+	if (arr[x][y]) return arr[x][y];
+	if (y == 0 || x == y) return 1;
+	return arr[x][y] = memo(x - 1, y - 1) + memo(x - 1, y);
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int n, r;
+	cin >> n >> r;
+
+	cout << memo(n, r);
+
+	return 0;
+}
