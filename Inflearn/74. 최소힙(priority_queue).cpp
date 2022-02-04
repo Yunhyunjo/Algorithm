35ad949473718e0685e3c4caf188d06cdf7ec0d2
Yunhyunjo <gotqlc147@likelion.org>
2022-02-04 17:14:10 +0900

    0204 - AC/REF

---
+++ Inflearn/74. 최소힙(priority_queue).cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+/*
+최소힙을 구현하는 방법은 두 가지가 있다.
+1. priority_queue <int> q 이렇게 최대 힙을 선언한 뒤 -를 붙여 삽입하기.
+2. priority_queue <int, vector <int>, greater <int>> q 이렇게 직접 최소 힙 선언하기.
+*/
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int n;
+	priority_queue <int, vector<int>, greater<int>> q;
+	while (1) {
+		cin >> n;
+		if (n == 0) {
+			if (q.empty()) cout << -1 << "\n";
+			else {
+				cout << q.top();
+				q.pop();
+			}
+		}
+		else if (n == -1) break;
+		else {
+			q.push(n);
+		}
+	}
+
+	return 0;
+}
